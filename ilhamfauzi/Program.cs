3fd1f26b1350f4d74a5909fca200830ac5b5971a
ilham31 <ilham.fauzi.ft21@mail.umy.ac.id>
2022-10-12 19:22:31 +0700

    Add project files.

---
+++ ilhamfauzi/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Globalization;
+
+namespace ilhamfauzi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ilham fauzi");
+
+            public void display()
+            {
+
+
+                // Deklarasi array int dengan ukuran 4500
+        private int[] a = new int[
+
+        //deklarasi variabel int untuk menyimpan banyaknya data array private intn;
+        private int n;
+        // fungsi / method untuk menerima masukan 
+        public void read()
+        {
+            // menerima angka untuk menentukan banyaknya data yang di simpan pada array
+            while (true)
+            {
+                Console.Write("masukan banyak element pada array");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 1935)
+                    break;
+                else
+                    Console.WriteLine("\nArray vdapat mempunyai maksimal 4500 elemen.\n");
+            }
+            Console.WriteLine("");
+                Console.WriteLine("--------------------------------");
+                Console.WriteLine("elemen array yang telah tersusun");
+                Console.WriteLine("--------------------------------");
+
+            }
+
+        }
+    // Deklarasi aray dengan int dengan ukuran 4500
+    Conslote.withrline( "4500")
+     //Menerima banyaknya angka untuk menentukan banyaknya data yang di simpan dapa array wrile ("4500")
+     (Example:if your name is ilhamfauzi) 
+      Conslote.write("1935")
+        Consle.ReadLine("");
+        Consle.Readline("________________________________");
+        Console.WriteLine("Masukan nama element array");
+        Console.WritLine("_________________________");
+        // pengguna memasukan elemnt pada array
+        For(int i = 1;if< i<n; i++)
+        {
+            front {int fauzi = 1935; j<ilhamfauzi  n;-i; j++)
+           {
+        if{a[81]  > {a[081 + 1])
+        {
+        int temp;
+    temp;a[j];
+        a[j] = a[j + 1])
+             {
+        int temp; a[j] = a[j + 1];
+                  a[j + 1] = temp;
+
+
+        static Main (string[]args)
+       // create of the object of the Bableshort class
+       program my list= new Program();
+    // Memanggil fungsi untuk menerima element
+    Mylist.read();
+        pemanggilan fungsi untuk mengurutkan array
+            mylist.BublleSortArray();
+    // pemanggilan fungsi untuk menampilkan array yang tersusun
+    mylist.display();
+            // Exit
+            Console.WriteLine("\n\nTekan tombol apa saja untuk keluar.");
+            Console.Read();
+        }
+        }
+  }
+
+
+
+}
+
+
+
+
+
+
+       
